0a1443939ed7fe330b3ef47b216c123816ab8c03
mrssz <1069889425@qq.com>
2020-11-15 21:35:20 +0800

    feat(Init):Init oneQuestion

---
+++ src/main/java/com/mrssz/onequestion/OnequestionApplication.java
@@ -0,0 +1,13 @@
+package com.mrssz.onequestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnequestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnequestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrssz/onequestion/OnequestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrssz.onequestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnequestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
