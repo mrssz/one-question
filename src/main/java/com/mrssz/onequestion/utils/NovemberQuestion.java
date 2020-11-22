898da809172e1fad99a19e4efbe4f1819e05d143
mrssz <1069889425@qq.com>
2020-11-22 23:48:51 +0800

    feat(U):好数对

--- src/main/java/com/mrssz/onequestion/utils/NovemberQuestion.java
+++ src/main/java/com/mrssz/onequestion/utils/NovemberQuestion.java
@@ -1,5 +1,8 @@
 package com.mrssz.onequestion.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NovemberQuestion {
     public static void main(String[] args) {
         System.out.println(reverseOnlyLetters("a-bC-dEf-ghIj"));
@@ -40,6 +43,24 @@ public class NovemberQuestion {
             }
         }
         return numbers[low];
+    }
+
+    /**
+     * 好数对
+     */
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            m.put(num, m.getOrDefault(num, 0) + 1);
+        }
+
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+            int v = entry.getValue();
+            ans += v * (v - 1) / 2;
+        }
+
+        return ans;
     }
 
 }
